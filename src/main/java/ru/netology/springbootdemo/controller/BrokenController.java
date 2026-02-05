274a505643af9ef6d30c2916ae8e6e5c103a8c53
TMAdevby <mtipun@mail.ru>
2026-02-05 12:48:39 +0300

    exceptions2

--- src/main/java/ru/netology/springbootdemo/controller/BrokenController.java
+++ src/main/java/ru/netology/springbootdemo/controller/BrokenController.java
@@ -1,4 +1,44 @@
 package ru.netology.springbootdemo.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import ru.netology.springbootdemo.domain.Person;
+
+@RestController
+@RequestMapping("/error")
 public class BrokenController {
+    @GetMapping("/error")
+    public String error(){
+        System.out.println("Logic");
+        throw new ResponseStatusException(HttpStatus.I_AM_A_TEAPOT,"ResponseStatusException");
+    }
+
+    @GetMapping("/{id}")
+    public Person getPersonById(@PathVariable("id") long id){
+        try{
+            throw new IllegalArgumentException("IllegalArgumentException");
+        }catch (RuntimeException e){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"ResponseStatusException",e);
+        }
+    }
+
+    @GetMapping("/iae")
+    public Person throwException() {
+        throw new IllegalArgumentException("throwException");
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> iaeHeandler(IllegalArgumentException e){
+        return new ResponseEntity<>("Exception in throwException method", HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> reHeandler(RuntimeException e){
+        return new ResponseEntity<>("Exception in throwException method", HttpStatus.I_AM_A_TEAPOT);
+    }
+
 }
+
+
